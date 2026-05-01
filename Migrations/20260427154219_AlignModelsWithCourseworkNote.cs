cf6038a9bd333f9706c8d6705c0a0aec0631c11e
Mykhailo Yevchuk <mievvchuk@users.noreply.github.com>
2026-05-01 09:48:24 +0300

    feat(migrations): add 20260427154219_AlignModelsWithCourseworkNote migration artifact

---
+++ Migrations/20260427154219_AlignModelsWithCourseworkNote.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AisVacanciesAndResumes.Migrations
+{
+    /// <inheritdoc />
+    public partial class AlignModelsWithCourseworkNote : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
