32c46d8fc269abe4b4ae7a6c4b6e88277cae8ab8
Mykhailo Yevchuk <mievvchuk@users.noreply.github.com>
2026-05-01 09:48:20 +0300

    feat(migrations): add 20260419085448_FinalCourseworkFixes migration artifact

---
+++ Migrations/20260419085448_FinalCourseworkFixes.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AisVacanciesAndResumes.Migrations
+{
+    /// <inheritdoc />
+    public partial class FinalCourseworkFixes : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "EmploymentType",
+                table: "Resumes",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CoverLetter",
+                table: "Applications",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "EmploymentType",
+                table: "Resumes");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CoverLetter",
+                table: "Applications",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+        }
+    }
+}
